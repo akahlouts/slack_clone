5c87f5441f429a6b52b17491688892ebdf3c61f1
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-09-14 02:09:08 +0300

    add full name input & its functions

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,18 @@
 import { convexAuth } from "@convex-dev/auth/server";
+import { DataModel } from "./_generated/dataModel";
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { Password } from "@convex-dev/auth/providers/Password";
 
+const CustomPassword = Password<DataModel>({
+  profile(params) {
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    };
+  },
+});
+
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Password, GitHub, Google],
+  providers: [CustomPassword, GitHub, Google],
 });
