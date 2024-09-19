0d1ef6a07e21216239be848fad9be43a61d14236
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-09-19 05:44:58 +0300

    implement handleComplete functionality on joinPage

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,22 +1,54 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { useJoin } from "@/features/workspaces/api/use-join";
+
+import { toast } from "sonner";
 
 import { Loader } from "lucide-react";
 import { FaSlackHash } from "react-icons/fa";
 
 import VerificationInput from "react-verification-input";
 
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 
 const JoinPage = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
 
   const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+  const { mutate, isPending } = useJoin();
+
+  const isMember = useMemo(() => data?.isMember, [data?.isMember]);
+
+  useEffect(() => {
+    if (isMember) {
+      router.push(`/workspace/${workspaceId}`);
+    }
+  }, [isMember, router, workspaceId]);
+
+  const handleComplete = (value: string) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace(`/workspace/${id}`);
+          toast.success("Workspace joined");
+        },
+        onError: () => {
+          toast.error("Failed to join workspace");
+        },
+      }
+    );
+  };
 
   if (isLoading) {
     return (
@@ -37,9 +69,13 @@ const JoinPage = () => {
           </p>
         </div>
         <VerificationInput
+          onComplete={handleComplete}
           length={6}
           classNames={{
-            container: "flex gap-x-2",
+            container: cn(
+              "flex gap-x-2",
+              isPending && "opacity-50 cursor-not-allowed"
+            ),
             character:
               "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
             characterInactive: "bg-muted",
