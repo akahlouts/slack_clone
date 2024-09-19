e4b379f27792cbfa5db8c79f9b456c30158663ef
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-09-19 04:50:20 +0300

    add join page ui

---
+++ src/app/join/[workspaceId]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+
+import { FaSlackHash } from "react-icons/fa";
+
+import VerificationInput from "react-verification-input";
+
+import { Button } from "@/components/ui/button";
+
+const JoinPage = () => {
+  return (
+    <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
+      <FaSlackHash size={60} className="text-rose-600" />
+      <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
+        <div className="flex flex-col gap-y-2 items-center justify-center">
+          <h1 className="text-2xl font-bold">Join workspace</h1>
+          <p className="text-muted-foreground">
+            Enter the workspace code to join
+          </p>
+        </div>
+        <VerificationInput
+          length={6}
+          classNames={{
+            container: "flex gap-x-2",
+            character:
+              "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black",
+          }}
+          autoFocus
+        />
+      </div>
+      <div className="flex gap-x-4">
+        <Button size="lg" variant="outline" asChild>
+          <Link href="/">Back to home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default JoinPage;
