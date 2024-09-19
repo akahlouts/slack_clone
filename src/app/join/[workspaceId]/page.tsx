1c6688feaf76f4d9df9f2bfb24771f7ae6cdecb8
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-09-19 05:19:11 +0300

    implement getWorkspaceInfo functionality

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -2,6 +2,11 @@
 
 import Link from "next/link";
 
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+
+import { Loader } from "lucide-react";
 import { FaSlackHash } from "react-icons/fa";
 
 import VerificationInput from "react-verification-input";
@@ -9,12 +14,24 @@ import VerificationInput from "react-verification-input";
 import { Button } from "@/components/ui/button";
 
 const JoinPage = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
   return (
     <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
       <FaSlackHash size={60} className="text-rose-600" />
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-          <h1 className="text-2xl font-bold">Join workspace</h1>
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
           <p className="text-muted-foreground">
             Enter the workspace code to join
           </p>
