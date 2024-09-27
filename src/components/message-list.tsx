33f42e14ff8599554b006fec0facd8b442bb5e98
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-09-27 21:31:28 +0300

    implement message, renderer & thumbnail components

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,8 +1,10 @@
-import { format, isToday, isYesterday } from "date-fns";
+import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 
 import { Message } from "./message";
+
+const TIME_THRESHOLD = 5;
 
 interface MessageListProps {
   memberName?: string;
@@ -61,6 +63,15 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => {
+            const prevMessage = messages[index - 1];
+            const isCompact =
+              prevMessage &&
+              prevMessage.user?._id === message.user?._id &&
+              differenceInMinutes(
+                new Date(message._creationTime),
+                new Date(prevMessage._creationTime)
+              ) < TIME_THRESHOLD;
+
             return (
               <Message
                 key={message._id}
@@ -76,7 +87,7 @@ export const MessageList = ({
                 createdAt={message._creationTime}
                 isEditing={false}
                 setEditingId={() => {}}
-                isCompact={false}
+                isCompact={isCompact}
                 hideThreadButton={false}
                 threadCount={message.threadCount}
                 threadImage={message.threadImage}
